160b3fe074d1a845518c725d4cb7d28c1ef63d82
Feha <>
2022-10-01 17:02:55 +0200

    Implement Player Shooting

--- src/entities/Player.gd
+++ src/entities/Player.gd
@@ -3,6 +3,12 @@ extends KinematicBody2D
 
 
 export var speed = 500
+export var bullet_speed = 10
+export var rof = 2
+
+var Bullet = preload("res://src/entities/Bullet.tscn")
+var last_shot = 0
+var look_dir = Vector2(1,0)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,16 +16,25 @@ func _ready():
   pass # Replace with function body.
 
 func _physics_process(delta):
+  if Engine.editor_hint:
+    return
+  
   var dir = Vector2()
-  if not Engine.editor_hint:
-    dir.x -= Input.get_action_strength("move_left")
-    dir.x += Input.get_action_strength("move_right")
-    dir.y -= Input.get_action_strength("move_up")
-    dir.y += Input.get_action_strength("move_down")
+  dir.x -= Input.get_action_strength("move_left")
+  dir.x += Input.get_action_strength("move_right")
+  dir.y -= Input.get_action_strength("move_up")
+  dir.y += Input.get_action_strength("move_down")
+  if dir.x != 0 or dir.y != 0:
+    look_dir = dir
   
-  var offset = speed * dir
+  var vel = speed * dir
+  self.move_and_slide(vel)
   
-  self.translate(delta * offset)
+  var cooldown = 1.0/rof
+  last_shot += delta
+  if Input.is_action_pressed("shoot") and last_shot >= cooldown:
+    last_shot = 0
+    shoot()
 
 
 func _draw():
@@ -28,3 +43,18 @@ func _draw():
   var dim = shape.shape.extents
   var col = Color(1, 0, 0, 1)
   draw_rect(Rect2(pos, dim), col)
+
+
+func shoot():
+  var aim_dir = Vector2()
+  aim_dir.x -= Input.get_action_strength("aim_left")
+  aim_dir.x += Input.get_action_strength("aim_right")
+  aim_dir.y -= Input.get_action_strength("aim_up")
+  aim_dir.y += Input.get_action_strength("aim_down")
+  if aim_dir.x == 0 and aim_dir.y == 0:
+    aim_dir = look_dir
+  aim_dir = aim_dir.normalized()
+  
+  var b = Bullet.instance()
+  b.start(self.transform.origin, aim_dir, bullet_speed)
+  get_parent().add_child(b)
