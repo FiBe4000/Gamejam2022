322238181b208fa2aa4873918de45e880adbb7a9
Jonathan Olsson <jonathan.olsson@wirelesscar.com>
2022-10-01 21:52:35 +0200

    Add additional functionallity to player and created game loop script

---
+++ src/Main.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var score
+
+
+func _ready():
+    randomize()
+    new_game()
+
+func _process(delta):
+  if Input.is_action_pressed("exit_game"):
+    exit_game()
+  
+func game_over():
+    pass
+
+func new_game():
+    score = 0
+    $Player.start($StartPosition.position)
+
+func exit_game():
+    get_tree().quit()
+
+func _on_ScoreTimer_timeout():
+    score += 1
+    $HUD.update_score(score)
+
+func _on_StartTimer_timeout():
+    $MobTimer.start()
+    $ScoreTimer.start()
+
+
+func _on_PlayerScene_hit():
+  game_over()
+
+
+func _on_HUD_start_game():
+  new_game()
--- src/entities/Player.gd
+++ src/entities/Player.gd
@@ -1,4 +1,3 @@
-tool
 extends KinematicBody2D
 
 
@@ -13,12 +12,22 @@ var look_dir = Vector2(1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  #hide()
+  pass
 
 func _physics_process(delta):
   if Engine.editor_hint:
     return
   
+  movement()
+  
+  var cooldown = 1.0/rof
+  last_shot += delta
+  if Input.is_action_pressed("shoot") and last_shot >= cooldown:
+    last_shot = 0
+    shoot()
+
+func movement():
   var dir = Vector2()
   dir.x -= Input.get_action_strength("move_left")
   dir.x += Input.get_action_strength("move_right")
@@ -30,20 +39,24 @@ func _physics_process(delta):
   var vel = speed * dir
   self.move_and_slide(vel)
   
-  var cooldown = 1.0/rof
-  last_shot += delta
-  if Input.is_action_pressed("shoot") and last_shot >= cooldown:
-    last_shot = 0
-    shoot()
-
-
-func _draw():
-  var shape = self.get_node("CollisionShape2D")
-  var pos = shape.transform.origin
-  var dim = shape.shape.extents
-  var col = Color(1, 0, 0, 1)
-  draw_rect(Rect2(pos-dim, dim*2), col)
-
+  if vel.length() > 0:
+    $AnimatedSprite.play()
+  else:
+    $AnimatedSprite.stop()
+  
+  if dir.x != 0:
+    $AnimatedSprite.animation = "walk"
+    $AnimatedSprite.flip_v = false
+    # See the note below about boolean assignment.
+    $AnimatedSprite.flip_h = dir.x < 0
+      
+func _on_Player_body_entered(body):
+  emit_signal("hit")
+      
+func start(pos):
+  position = pos
+  show()
+  $CollisionShape2D.disabled = false
 
 func shoot():
   var aim_dir = Vector2()
