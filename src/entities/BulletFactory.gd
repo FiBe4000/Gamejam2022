d06d4dd65dbe55e6f8ed92349310e4c9e1bfcb68
Jonathan Olsson <jonathan.olsson@wirelesscar.com>
2022-10-02 20:04:48 +0200

    Extended bullet factory and player is using it.

--- src/entities/BulletFactory.gd
+++ src/entities/BulletFactory.gd
@@ -1,20 +1,27 @@
 extends Node
 
+var Bullet = preload("res://src/entities/Bullet.tscn")
+
 func _ready():
   pass
 
+func _on_shoot(sourceType, pos, dir, speed, damage, scale):
+  
+  var sourceCollisionLayer = 0
+  match(sourceType):
+    "Player": 
+      sourceCollisionLayer = 2
+    "Mob":
+      sourceCollisionLayer = 3
+    _:
+      sourceCollisionLayer = 0
+      
+  var collisionMask = inverse_mask_for_layer(sourceCollisionLayer)
+  var newBullet = Bullet.instance()
+  newBullet.createAndShoot(pos, dir, speed, damage, scale, collisionMask)
+  self.add_child(newBullet)
 
-func _on_shoot(sourceType, position, direction, speed, damage, size):
-  
-  var aim_dir = Vector2()
-  aim_dir.x -= Input.get_action_strength("aim_left")
-  aim_dir.x += Input.get_action_strength("aim_right")
-  aim_dir.y -= Input.get_action_strength("aim_up")
-  aim_dir.y += Input.get_action_strength("aim_down")
-  if aim_dir.x == 0 and aim_dir.y == 0:
-    aim_dir = look_dir
-  aim_dir = aim_dir.normalized()
-  
-  var b = Bullet.instance()
-  b.start(self.transform.origin, aim_dir, bullet_speed)
-  get_parent().add_child(b)
+func inverse_mask_for_layer(layer):
+  if layer == 0:
+    return 0
+  return 255 - pow(2, layer - 1)
