7481f4d303e88db265dbef0c158a499db18f6d80
Feha <>
2022-10-03 18:45:21 +0200

    Add variability to constant alignment-change

--- src/Main.gd
+++ src/Main.gd
@@ -1,6 +1,13 @@
 extends Node
 
-const change_period = PI / 20 # half turn / seconds
+const change_period = PI / 100 # half turn / seconds
+const variability_period = PI / (1 * 1000) # half turn / (seconds * milliseconds)
+const variability_strength = 0.1
+const variability_friction_str = 10
+
+var variability_vel = 0
+var lowest = variability_vel
+var highest = variability_vel
 
 var score
 
@@ -18,7 +25,12 @@ func update_health_bar():
   $HUD_Scene.update_player_health(($Player.get_health()))
 
 func update_world_alignment(delta):
-  var rot = change_period
+  var variability_acc = 1*sin(float(OS.get_ticks_msec()) * variability_period) + 20*rand_range(-1.0,1.0)
+  variability_vel += variability_acc * delta
+  var variability_friction = 1 + variability_friction_str * abs(variability_vel) * delta
+  variability_vel /= variability_friction
+  #variability_vel = sign(variability_vel) * sqrt(abs(variability_vel))
+  var rot = change_period + variability_vel*variability_strength
   $WorldChangeSystem.rotate(rot * delta, -1)
 
 func game_over():
