1a865c31d78f82ad1c34b8037c04baa212d995bc
Johan Toft <johan.toft@gmail.com>
2022-10-03 23:02:52 +0200

    Rain in dark and fey worlds

--- src/HUD/HUD.gd
+++ src/HUD/HUD.gd
@@ -1,6 +1,13 @@
 extends CanvasLayer
 
+const WS = preload("res://src/WorldChangeSystem.gd")
 
+var rain_enabled = {
+  WS.World.NORMAL: false,
+  WS.World.DARK: true,
+  WS.World.FIRE: false,
+  WS.World.ICE: true,
+ }
 
 func _ready():
   $ScoreNumber.visible = false
@@ -21,3 +28,7 @@ func update_player_health(health):
 func _on_Player_death():
   $ScoreNumber.text = str(ceil(0))
   $DeahtNotice.visible = true
+
+
+func _on_WorldChangeSystem_next_world_change(new_world):
+  $Weather.visible = rain_enabled[new_world]
